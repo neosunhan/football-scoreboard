d63d3a0b853e076eb8aa84425ca9e0b40d546d17
neosunhan <neosunhan@gmail.com>
2024-09-20 00:20:33 +0800

    Add db

---
+++ app/models.py
@@ -0,0 +1,25 @@
+import datetime
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
+
+class Team(db.Model):
+    # id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(64), primary_key=True)
+    registration_date: so.Mapped[datetime.datetime] = so.mapped_column(default=lambda: datetime.datetime.now())
+    group: so.Mapped[int] = so.mapped_column()
+    matches: so.WriteOnlyMapped['Match'] = so.relationship(back_populates='team')
+
+    def __repr__(self):
+        return f"Team {self.name}, Group {self.group}, Registered on {self.registration_date}"
+    
+class Match(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    team_a: so.Mapped[str] = so.mapped_column(sa.ForeignKey(Team.name))
+    team_b: so.Mapped[str] = so.mapped_column(sa.ForeignKey(Team.name))
+    goals_a: so.Mapped[int] = so.mapped_column()
+    goals_b: so.Mapped[int] = so.mapped_column()
+    team: so.Mapped[Team] = so.relationship(back_populates='matches')
+
+    def __repr__(self):
+        return f"{self.team_a} {self.goals_a} - {self.team_b} {self.goals_b}"
--- config.py
+++ config.py
@@ -3,4 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('FLASK_SECRET_KEY') or 'scoreboard-secret'
-    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
