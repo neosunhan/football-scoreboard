8207546531f7725eb2878d90c1cc59d3c3786508
neosunhan <neosunhan@gmail.com>
2024-09-20 19:22:49 +0800

    Add nav bar

--- app/forms.py
+++ app/forms.py
@@ -3,12 +3,9 @@ from wtforms import TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class TeamsForm(FlaskForm):
-    teams = TextAreaField('Teams Input', validators=[Length(min=1)])
+    teams = TextAreaField('Teams Input', validators=[DataRequired(), Length(min=1)])
     submit = SubmitField('Submit Teams')
 
 class MatchesForm(FlaskForm):
-    matches = TextAreaField('Matches Input', validators=[Length(min=1)])
+    matches = TextAreaField('Matches Input', validators=[DataRequired(), Length(min=1)])
     submit = SubmitField('Submit Matches')
-
-class ClearForm(FlaskForm):
-    submit = SubmitField('Clear all data')
--- app/routes.py
+++ app/routes.py
@@ -1,31 +1,34 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import TeamsForm, MatchesForm, ClearForm
+from app.forms import TeamsForm, MatchesForm
 from app.models import get_groups, clear_data
 from app.parser import parse_teams, parse_matches
-import datetime
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    teams_form = TeamsForm()
-    matches_form = MatchesForm()
-    # clear_form = ClearForm()
-    # if clear_form.validate_on_submit():
-    #     clear_data()
-    #     redirect(url_for('index'))
-    if teams_form.validate_on_submit():
-        parse_teams(teams_form.teams.data)
-        flash(f"teams: {teams_form.teams.data}")
-        redirect(url_for('index'))
-    if matches_form.validate_on_submit():
-        parse_matches(matches_form.matches.data)
-        flash(f"matches: {matches_form.matches.data}")
-        redirect(url_for('index'))
-    return render_template('index.html', teams=get_groups(), teams_form=teams_form, matches_form=matches_form)
+    groups = get_groups()
+    return render_template('index.html', teams=groups)
 
 @app.route('/clear', methods=['POST'])
 def clear():
     clear_data()
     return redirect(url_for('index'))
     
+@app.route('/teams', methods=['GET', 'POST'])
+def teams():
+    teams_form = TeamsForm()
+    if teams_form.validate_on_submit():
+        parse_teams(teams_form.teams.data)
+        flash(f"teams: {teams_form.teams.data}")
+        return redirect(url_for('index'))
+    return render_template('teams.html', teams_form=teams_form)
+    
+@app.route('/matches', methods=['GET', 'POST'])
+def matches():
+    matches_form = MatchesForm()
+    if matches_form.validate_on_submit():
+        parse_matches(matches_form.matches.data)
+        flash(f"matches: {matches_form.matches.data}")
+        return redirect(url_for('index'))
+    return render_template('matches.html', matches_form=matches_form)
